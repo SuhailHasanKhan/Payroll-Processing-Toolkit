e7112ca35dc48970ac9c44992f15d68edf37ce3b
Suhail Hasan Khan <shasankhan@usf.edu>
2025-09-25 20:28:14 -0400

    created an array for employees

--- cc_5.js
+++ cc_5.js
@@ -1,2 +1,9 @@
 // Coding Challenge 00
 
+let employees = [
+    {name: 'James Bond', hourlyRate: 75, hoursWorked: 35},
+    {name: 'Bob Dylan', hourlyRate: 40, hoursWorked: 20},
+    {name: 'Paul Tall', hourlyRate: 90, hoursWorked: 92},
+    {name: 'Ranbir Kapoor', hourlyRate: 15, hoursWorked: 67}
+];
+
