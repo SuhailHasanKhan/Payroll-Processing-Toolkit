f9171a0c7f055e61d7ff22995748caf697b0e854
Suhail Hasan Khan <shasankhan@usf.edu>
2025-09-27 18:26:18 -0400

    Made a function for base pay

--- cc_5.js
+++ cc_5.js
@@ -1,4 +1,4 @@
-// Coding Challenge 00
+// Coding Challenge 05
 
 let employees = [
     {name: 'James Bond', hourlyRate: 75, hoursWorked: 35},
@@ -7,3 +7,7 @@ let employees = [
     {name: 'Ranbir Kapoor', hourlyRate: 15, hoursWorked: 67}
 ];
 
+function calculateBasePay(rate, hours) {
+    let baseHours = (0, hours, 40);
+    return rate*baseHours;
+}
